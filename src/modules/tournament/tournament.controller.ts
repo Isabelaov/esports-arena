b8adbc0e5c3780296e354ccfd3c163f952d2648c
Isabela Ortega Vasco <isabela2603ov@gmail.com>
2024-10-07 17:01:16 -0500

    feat: documentation for players and tournaments

--- src/modules/tournament/tournament.controller.ts
+++ src/modules/tournament/tournament.controller.ts
@@ -11,27 +11,38 @@ import { TournamentService } from './tournament.service';
 import { CreateTournamentDto } from './dto/create-tournament.dto';
 import { UpdateTournamentDto } from './dto/update-tournament.dto';
 import { ApiTags } from '@nestjs/swagger';
+import {
+  CreateTournament,
+  DeleteTournament,
+  FindOneTournament,
+  FindTournaments,
+  UpdateTournament,
+} from './docs/tournament.doc';
 
 @ApiTags('Tournaments')
 @Controller('tournament')
 export class TournamentController {
   constructor(private readonly tournamentService: TournamentService) {}
 
+  @CreateTournament()
   @Post()
   create(@Body() createTournamentDto: CreateTournamentDto) {
     return this.tournamentService.create(createTournamentDto);
   }
 
+  @FindTournaments()
   @Get()
   findAll() {
     return this.tournamentService.findAll();
   }
 
+  @FindOneTournament()
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tournamentService.findOne(+id);
   }
 
+  @UpdateTournament()
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -40,6 +51,7 @@ export class TournamentController {
     return this.tournamentService.update(+id, updateTournamentDto);
   }
 
+  @DeleteTournament()
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.tournamentService.remove(+id);
