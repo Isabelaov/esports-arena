006194f293bac74b88b0b1c79968737033f876f4
Isabela Ortega Vasco <isabela2603ov@gmail.com>
2024-10-07 15:22:08 -0500

    feat: create player entity

--- src/modules/player/entities/player.entity.ts
+++ src/modules/player/entities/player.entity.ts
@@ -1,1 +1,33 @@
-export class Player {}
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+  Entity,
+} from 'typeorm';
+
+@Entity('players')
+export class Player {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: '200' })
+  name: string;
+
+  @Column({ type: 'varchar', length: '200' })
+  email: string;
+
+  @Column({ type: 'varchar', length: '200', select: false })
+  password: string;
+
+  @Column({ type: 'varchar', length: '200', nullable: true })
+  address: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  format() {
+    this.name = this.name.trim().toLocaleUpperCase();
+    this.address = this.address?.trim().toLocaleUpperCase();
+    this.email = this.email.trim();
+  }
+}
